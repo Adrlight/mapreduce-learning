b33ef189fe2bb541677b5dcd8b52dd93b35a77a8
Adrlight <280243155@qq.com>
2019-01-28 23:59:54 +0800

    上传文件
    Bingo!

---
+++ src/main/java/adrlight/hadoop/mapreduce_learning/App.java
@@ -0,0 +1,13 @@
+package adrlight.hadoop.mapreduce_learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
